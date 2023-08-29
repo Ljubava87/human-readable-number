4b94c2f0f42cc3317340846fb70854e84935d66a
unknown <you@example.com>
2023-08-29 11:14:25 +0100

    feat:  human reaible nuber code

--- src/index.js
+++ src/index.js
@@ -1,50 +1,40 @@
-
 var units = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 var teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
 var tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
 
-
 function toReadable(number) {
+    if (typeof number !== 'number' || number < 0 || number >= 1000 || !Number.isInteger(number)) {
+        return number === 1000 ? 'one thousand' : 'Invalid input';
+    }
 
-if (typeof number !== 'number' || number < 0 || number >= 1000 || !Number.isInteger(number)) {
+    var numStr = number.toString();
+    var result = '';
 
-return number === 1000 ? 'one thousand' : 'Invalid input';
+    switch (numStr.length) {
+        case 1:
+            result = units[number];
+            break;
+        case 2:
+            if (number >= 10 && number <= 19) {
+                result = teens[number - 10];
+            } else {
+                result = tens[Math.floor(number / 10) - 2];
+                if (number % 10 !== 0) {
+                    result += ' ' + units[number % 10];
+                }
+            }
+            break;
+        case 3:
+            result = units[Math.floor(number / 100)] + ' hundred';
+            if (number % 100 !== 0) {
+                result += ' ' + toReadable(number % 100);
+            }
+            break;
+        default:
+            return 'Invalid input';
+    }
+
+    return result;
 }
 
-
-var numStr = number.toString();
-
-
-var result = '';
-
-
-switch (numStr.length) {
-
-case 1:
-result = units[number];
-break;
-
-case 2:
-if (number >= 10 && number <= 19) {
-
-result = teens[number - 10];
-} else {
-result = tens[Math.floor(number / 10) - 2];
-if (number % 10 !== 0) {
-result += '-' + units[number % 10];
-}
-}
-break;
-case 3:
-result = units[Math.floor(number / 100)] + ' hundred';
-if (number % 100 !== 0) {
-result += ' ' + toReadable(number % 100); }
-break;
-default:
-
-return 'Invalid input';
-}
-
-
-return result;
-}
+module.exports = toReadable;
